a2338aeb85296513f167da7452d91356db5e7bdc
AhmedAAAbd <ahmed.abdulrazzaq@lsicorp.com>
2022-12-11 19:20:31 -0500

    added checking for undefined props

--- resources/components/fabric-select.js
+++ resources/components/fabric-select.js
@@ -778,7 +778,7 @@ Polymer({
 				listItems.push(item);
 		});
 		//Added by AAAbd to sort the lists by names
-		listItems = listItems.sort((a,b)=>(a.name> b.name) ? 1 :-1)
+		listItems = listItems.sort((a,b)=>(a?.name> b?.name) ? 1 :-1)
 		this.set("listItems", listItems);
 		//console.log("this.selected", this.selected)
 		//console.log("listItems", this.listItems )
